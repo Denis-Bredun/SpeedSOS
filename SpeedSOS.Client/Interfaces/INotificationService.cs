e933bb9e1a84181634355b68a3d8d5fa1b54525d
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-03 10:13:43 +0300

    Added INotificationService

---
+++ SpeedSOS.Client/Interfaces/INotificationService.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Maui.Core;
+using SpeedSOS.Client.Constants;
+
+namespace SpeedSOS.Client.Interfaces
+{
+    public interface INotificationService
+    {
+        Task ShowToastAsync(
+            string message,
+            double textSize = DefaultArguments.ToastTextSize,
+            ToastDuration duration = DefaultArguments.DefaultToastDuration);
+
+        Task<bool> ShowYesNoDialogAsync(string message,
+            string title = DefaultArguments.ConfirmDialogTitle,
+            string yesText = DefaultArguments.ConfirmYes,
+            string noText = DefaultArguments.ConfirmNo);
+
+        Task<string> ShowActionSheetAsync(
+            string title = DefaultArguments.ActionSheetDefaultMessage,
+            string cancel = DefaultArguments.ActionSheetCancel,
+            params string[] options);
+    }
+}
