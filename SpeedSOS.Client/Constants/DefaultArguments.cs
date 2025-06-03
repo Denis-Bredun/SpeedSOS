57b8a8a66492e05f53039b0c5a5c3dbaeab37814
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-03 10:08:24 +0300

    Added class DefaultArguments where the values of the default arguments will be stored

---
+++ SpeedSOS.Client/Constants/DefaultArguments.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Maui.Core;
+
+namespace SpeedSOS.Client.Constants
+{
+    public static class DefaultArguments
+    {
+        public const string ConfirmDialogTitle = "Підтвердження";
+        public const string ConfirmYes = "Так";
+        public const string ConfirmNo = "Ні";
+        public const string ActionSheetCancel = "Нічого";
+        public const string ActionSheetDefaultMessage = "Що ви бажаєте зробити?";
+        public const double ToastTextSize = 20;
+        public const ToastDuration DefaultToastDuration = ToastDuration.Long;
+    }
+}
