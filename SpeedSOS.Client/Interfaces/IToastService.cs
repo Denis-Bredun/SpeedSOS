70b7f7b101f0644889a5bf7925122c19596ef22a
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-06 02:26:18 +0300

    Added ToastService

---
+++ SpeedSOS.Client/Interfaces/IToastService.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Maui.Core;
+using SpeedSOS.Client.Constants;
+
+namespace SpeedSOS.Client.Interfaces
+{
+    public interface IToastService
+    {
+        Task ShowToast(
+            string message,
+            double textSize = DefaultArguments.ToastTextSize,
+            ToastDuration duration = DefaultArguments.DefaultToastDuration);
+    }
+}
---
+++ SpeedSOS.Client/Services/ToastService.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using SpeedSOS.Client.Constants;
+using SpeedSOS.Client.Interfaces;
+
+namespace SpeedSOS.Client.Services
+{
+    public class ToastService : IToastService
+    {
+        public async Task ShowToast(
+            string message,
+            double textSize = DefaultArguments.ToastTextSize,
+            ToastDuration duration = DefaultArguments.DefaultToastDuration)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException("Message cannot be null or empty.", nameof(message));
+
+            if (textSize <= 0)
+                throw new ArgumentOutOfRangeException(nameof(textSize), "Text size must be greater than zero.");
+
+            await Toast.Make(message, duration, textSize).Show();
+        }
+    }
+}
