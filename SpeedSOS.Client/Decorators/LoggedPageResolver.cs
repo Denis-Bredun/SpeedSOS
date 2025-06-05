f062e72844c699d4cc701bbf1db1150985335944
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-06 01:54:33 +0300

    Added LoggedPageResolver

---
+++ SpeedSOS.Client/Decorators/LoggedPageResolver.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using SpeedSOS.Client.Interfaces;
+
+namespace SpeedSOS.Client.Decorators
+{
+    public class LoggedPageResolver : IPageResolver
+    {
+        private readonly IPageResolver _innerPageResolver;
+        private readonly ILogger<LoggedPageResolver> _logger;
+
+        public LoggedPageResolver(
+            IPageResolver innerPageResolver,
+            ILogger<LoggedPageResolver> logger)
+        {
+            _innerPageResolver = innerPageResolver;
+            _logger = logger;
+        }
+
+        public Page GetCurrentPage()
+        {
+            Page page;
+
+            try
+            {
+                page = _innerPageResolver.GetCurrentPage();
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogError(ex, "Failed to get current page from inner resolver.");
+                throw;
+            }
+            return page;
+        }
+    }
+}
