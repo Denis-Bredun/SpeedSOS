ea85d83bca9c884024f5a4ecbfab111bd75358bf
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-15 07:50:37 +0300

    Removed default values from DTOs for validation because default values are set in parameters in methods

--- SpeedSOS.Client/DTOs/Validation/ActionSheetRequest.cs
+++ SpeedSOS.Client/DTOs/Validation/ActionSheetRequest.cs
@@ -1,12 +1,10 @@
-﻿using SpeedSOS.Client.Constants;
-
-namespace SpeedSOS.Client.DTOs.Validation
+﻿namespace SpeedSOS.Client.DTOs.Validation
 {
     public class ActionSheetRequest
     {
-        public string Title { get; set; } = DefaultArguments.ActionSheetDefaultMessage;
-        public string Cancel { get; set; } = DefaultArguments.ActionSheetCancel;
-        public string Destruction { get; set; } = DefaultArguments.ActionSheetDestruction;
+        public string Title { get; set; } = null!;
+        public string Cancel { get; set; } = null!;
+        public string Destruction { get; set; } = null!;
         public string[] Options { get; set; } = Array.Empty<string>();
     }
 }
--- SpeedSOS.Client/DTOs/Validation/YesNoDialogRequest.cs
+++ SpeedSOS.Client/DTOs/Validation/YesNoDialogRequest.cs
@@ -1,12 +1,10 @@
-﻿using SpeedSOS.Client.Constants;
-
-namespace SpeedSOS.Client.DTOs.Validation
+﻿namespace SpeedSOS.Client.DTOs.Validation
 {
     public class YesNoDialogRequest
     {
         public string Message { get; set; } = null!;
-        public string Title { get; set; } = DefaultArguments.ConfirmDialogTitle;
-        public string YesText { get; set; } = DefaultArguments.ConfirmYes;
-        public string NoText { get; set; } = DefaultArguments.ConfirmNo;
+        public string Title { get; set; } = null!;
+        public string YesText { get; set; } = null!;
+        public string NoText { get; set; } = null!;
     }
 }
